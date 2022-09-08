bdd149c35d291c4901bb1810d575cbab38903a3e
TTiande <984109188@qq.com>
2022-09-08 17:43:09 +0800

    验证回文串

---
+++ ConsoleTest/ConsoleTest/IsPalindrome.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Collections;
+
+namespace ConsoleTest
+{
+    class IsPalindrome
+    {
+        public bool isPalindrome(string s)
+        {
+            Char[] str;
+            Stack st = new Stack();
+
+            //去掉所有空白；
+            s.Replace(" ", "");
+            //利用栈插入所有的字母数字
+            str = s.ToCharArray();
+
+        }
+    }
+}
