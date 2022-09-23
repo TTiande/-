3f6317fcffa3fb91a9528fb971296e865f5f02dd
TTiande <984109188@qq.com>
2022-09-23 21:30:11 +0800

    重复的子字符串

--- ConsoleTest/ConsoleTest/RepeatedSubstringPattern.cs
+++ ConsoleTest/ConsoleTest/RepeatedSubstringPattern.cs
@@ -10,16 +10,21 @@ namespace ConsoleTest
         public bool repeatedSubstringPattern(string s)
         {//思路1：长度为0或1返回false。重复与首字母进行判断，用队列最方便。
             //思路2：用Compare方法或Equals方法进行判断,反复比较。
-            StringBuilder str = new StringBuilder(s);
-            Stack<char> temp = new Stack<char>();
+            StringBuilder str = new StringBuilder();
+            StringBuilder temp = new StringBuilder();
             if (s.Length < 2) return false;
-            for (int i = 0; i < s.Length; i++)
+            for (int i = 0; i < s.Length/2+1; i++)
             {
-                if (temp.Contains(s[i])) { temp.Push(s[i]); continue; }
-                string temp_Str=temp.ToString();
+                if (!temp.ToString().Contains(s[i])) { temp.Append(s[i]); continue; }
+                string temp_Str= temp.ToString();
+                temp.Append(s[i]);
                 if (s.Length % temp_Str.Length != 0) continue;
+                str=str.Clear();
+                for (int j = 0; j < s.Length / temp_Str.Length; j++) str=str.Append(temp_Str);
                 
+                if (str.ToString() == s) return true; else continue;
             }
+            return false;
             //实现1
             //StringBuilder str = new StringBuilder();
             //if (s.Length < 2) return false;
