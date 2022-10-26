22435ae66ee331f99378c3768826766419027992
TTiande <984109188@qq.com>
2022-10-26 17:01:19 +0800

    83.删除排序链表中的重复元素

--- ConsoleTest/DataStructure/DeleteDuplicates.cs
+++ ConsoleTest/DataStructure/DeleteDuplicates.cs
@@ -24,15 +24,45 @@ namespace DataStructure
             List<int> num = new List<int>();
             try
             {
-                if (pointer.next == null || pointer == null) return head;
+                if (pointer == null || pointer.next == null) return head;
                 num.Add(pointer.val);
                 while (pointer.next != null && pointer != null)
                 {
                     if (num.Contains(pointer.next.val))
                     {
-                        pointer.next = pointer.next.next;
-                        pointer = pointer.next;
-                        num.Add(pointer.val);
+                        bool ctr = true;
+                        ListNode temp = pointer.next;
+                        while (ctr)
+                        {
+                            if (!num.Contains(temp.val))
+                            {
+                                pointer.next = temp;
+                                pointer = pointer.next;
+                                num.Add(pointer.val);
+                                ctr = false;
+                            }
+                            else
+                            {
+                                if (temp.next != null)
+                                    temp = temp.next;
+                                else
+                                {
+                                    ctr = false;
+                                    pointer.next = null;
+                                }
+
+                            }
+                        }
+                        // if(pointer.next.next!=null){
+
+                        //     pointer.next=pointer.next.next;
+                        //     pointer=pointer.next;
+                        //     num.Add(pointer.val);
+                        // }
+                        // else{
+                        //     pointer.next=null;
+                        //     break; 
+                        // }              
                     }
                     else
                     {
