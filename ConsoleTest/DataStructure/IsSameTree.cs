5ce3a6b5133b03175aea18f490526b5d76557224
TTiande <984109188@qq.com>
2022-10-27 17:48:30 +0800

    100.相同的树

---
+++ ConsoleTest/DataStructure/IsSameTree.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DataStructure
+{
+    class IsSameTree
+    {
+        public bool IsSameTree(TreeNode p, TreeNode q)
+        {
+            if (p != null || q != null)
+            {
+                if (p.val != q.val) return false;
+            }
+            if (p.left != null || q.left != null)
+            {
+                res = IsSameTree(p.left, q.left);
+            }
+            if (res != true) return false;
+            if (p.right != null || q.right != null)
+            {
+                res = IsSameTree(p.right, q.right);
+            }
+            if (res != true) return false;
+            return true;
+        }   
+    }
+}
