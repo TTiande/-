dc465104d6a2bad4961c1e54620518997ed82de9
TTiande <984109188@qq.com>
2022-11-01 17:32:21 +0800

    112.路经总和

---
+++ ConsoleTest/DataStructure/HasPathSum.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DataStructure
+{//112.路径总和
+    class HasPathSum
+    {
+        bool res = false;
+        int sum = 0;
+        public bool hasPathSum(TreeNode root, int targetSum)
+        {
+            if (root == null) return false;
+            sum = root.val;
+            if (root.left != null) caculate(root.left, targetSum);
+            if (root.right != null) caculate(root.right, targetSum);
+            if (root.left == null && root.right == null && sum == targetSum) res = true;
+
+            return res;
+        }
+        public void caculate(TreeNode root, int targetSum)
+        {
+            sum += root.val;
+            if (root.left == null && root.right == null)
+            {
+                if (sum == targetSum) res = true;
+            }
+            if (root.left != null) caculate(root.left, targetSum);
+            if (root.right != null) caculate(root.right, targetSum);
+            sum -= root.val;
+        }
+    }
+}
